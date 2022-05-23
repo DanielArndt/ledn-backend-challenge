7de6f100e82b38ad2c8469935f09a065de9a8cae
Daniel Arndt <dan@arndt.ca>
2022-05-23 11:00:37 -0300

    Connection to mongodb

---
+++ mongodb/import-data.sh
@@ -0,0 +1,2 @@
+mongoimport --jsonArray --db ledn --collection accounts --file /seed/accounts-api.json
+mongoimport --jsonArray --db ledn --collection transactions --file /seed/transactions-api.json
--- python-api/main.py
+++ python-api/main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
 import motor.motor_asyncio
 import os
+from pydantic import BaseModel, Field
+from typing import List
 
 app = FastAPI()
+client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+db = client.ledn
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+class AccountModel(BaseModel):
+    first_name: str = Field(alias="firstName")
+
+@app.get(
+    "/accounts", response_description="List all accounts", response_model=List[AccountModel]
+)
+async def list_accounts():
+    accounts = await db["accounts"].find().to_list(1000)
+    return accounts
+
